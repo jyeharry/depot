eebfd22edf1f60e3d5ae2e0e280c996cfe733a5f
Jye Harry <jyeharry21@hotmail.com>
2025-02-27 00:04:14 +1100

    D3 adding a button done

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    session[:view_count] = 0 if session[:view_count].nil?
+    session[:view_count] += 1
   end
 end
