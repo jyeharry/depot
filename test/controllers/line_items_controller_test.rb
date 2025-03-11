b6ce789d24212d168ee1380055020fe18ea9661a
Jye Harry <jyeharry21@hotmail.com>
2025-03-11 22:29:17 +1100

    Iteration F1 moving the cart done

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -43,7 +43,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should update line_item" do
     patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
-    assert_redirected_to line_item_url(@line_item)
+    assert_redirected_to cart_url(@line_item.cart)
   end
 
   test "should destroy line_item" do
@@ -51,6 +51,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
       delete line_item_url(@line_item)
     end
 
-    assert_redirected_to line_items_url
+    assert_redirected_to cart_url(@line_item.cart)
   end
 end
