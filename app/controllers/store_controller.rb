5497767685528490c5a1f9de4c01b96a58225525
Jye Harry <jyeharry21@hotmail.com>
2025-03-11 22:29:17 +1100

    Iteration F1 moving the cart done

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order(:title)
     session[:view_count] = 0 if session[:view_count].nil?
