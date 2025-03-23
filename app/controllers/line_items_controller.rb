dcbdb7b0c0565c43b3e6d62788a02384f8b6cf9c
Jye Harry <jyeharry21@hotmail.com>
2025-03-24 10:54:51 +1100

    Iteration F2 creating a hotwired cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,6 +30,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         session[:view_count] = 0
+        format.turbo_stream
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
       else
